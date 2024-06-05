c4648c4dca7bd20ab31fb23a74224eccebca3400
Alejandro Coca <alxmcr@outlook.com>
2024-06-05 02:07:11 -0400

    wip

--- src/components/page-results-flights/sections/HeroSectionResultsFlightsPage.tsx
+++ src/components/page-results-flights/sections/HeroSectionResultsFlightsPage.tsx
@@ -11,16 +11,18 @@ export default function HeroSectionResultsFlightsPage({ fromId = 0, toId = 0 }: 
   return (
     <section className="w-full bg-black py-[100px]">
       <div className="mx-auto h-[50px] lg:w-[1028px]">
-        <div className="flex flex-wrap items-center justify-center gap-5 px-4 md:px-10">
-          <BoxCityHeader cityId={2} />
-          <div className="lg:hidden">
-            <RouteIllustrationMobile />
+        {fromId > 0 && toId > 0 ? (
+          <div className="flex flex-wrap items-center justify-center gap-5 px-4 md:px-10">
+            <BoxCityHeader cityId={fromId} />
+            <div className="lg:hidden">
+              <RouteIllustrationMobile />
+            </div>
+            <div className="hidden md:flex">
+              <RouteIllustrationDesktop />
+            </div>
+            <BoxCityHeader cityId={toId} />
           </div>
-          <div className="hidden md:flex">
-            <RouteIllustrationDesktop />
-          </div>
-          <BoxCityHeader cityId={3} />
-        </div>
+        ) : null}
       </div>
     </section>
   );
