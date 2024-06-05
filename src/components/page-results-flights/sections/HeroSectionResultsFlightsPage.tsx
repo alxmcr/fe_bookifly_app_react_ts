2e49b70fa34c57e36458c8ecb4baf655c3373a33
Alejandro Coca <alxmcr@outlook.com>
2024-06-05 01:42:53 -0400

    wip

--- src/components/page-results-flights/sections/HeroSectionResultsFlightsPage.tsx
+++ src/components/page-results-flights/sections/HeroSectionResultsFlightsPage.tsx
@@ -9,9 +9,9 @@ export default function HeroSectionResultsFlightsPage({ fromId = 0, toId = 0 }: 
   return (
     <section className="w-full bg-black py-10">
       <div className="mx-auto h-[188px] lg:w-[1028px]">
-        <div className="px-4 md:px-10">
-          <BoxCityHeader cityId={fromId} />
-          <BoxCityHeader cityId={toId} />
+        <div className="flex flex-wrap justify-between px-4 md:px-10">
+          <BoxCityHeader cityId={2} />
+          <BoxCityHeader cityId={3} />
         </div>
       </div>
     </section>
