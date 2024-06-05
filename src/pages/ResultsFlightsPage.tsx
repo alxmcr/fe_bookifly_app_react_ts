1c44508d6425a069113aeaf7fc63def274ee30c4
Alejandro Coca <alxmcr@outlook.com>
2024-06-04 23:57:50 -0400

    wip

--- src/pages/ResultsFlightsPage.tsx
+++ src/pages/ResultsFlightsPage.tsx
@@ -4,6 +4,7 @@ export default function ResultsFlightsPage() {
   return (
     <div>
       <MainContentResultsFlightsPage />
+      <div className="h-screen w-full bg-[url('/assets/images/bg-main-bookifly.png')] bg-no-repeat"></div>
     </div>
   );
 }
