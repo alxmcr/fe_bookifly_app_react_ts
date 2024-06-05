d1873e1f575df21883b9a953ffd5e0e653d6617a
Alejandro Coca <alxmcr@outlook.com>
2024-06-04 23:54:14 -0400

    wip

--- src/components/page-results-flights/sections/HeroSectionResultsFlightsPage.tsx
+++ src/components/page-results-flights/sections/HeroSectionResultsFlightsPage.tsx
@@ -1,8 +1,8 @@
 export default function HeroSectionResultsFlightsPage() {
   return (
-    <section className="w-full bg-[url('/assets/images/bg-main-bookifly.png')] bg-no-repeat py-10">
-      <div className="mx-auto h-screen lg:w-[1028px]">
-        <div className="flex h-screen flex-col justify-center gap-[50px] px-4 md:items-center md:px-10">
+    <section className="w-full bg-black py-10">
+      <div className="mx-auto h-[188px] lg:w-[1028px]">
+        <div className="flex h-[188px] flex-col justify-center gap-[50px] px-4 md:items-center md:px-10">
           <h1 className="w-[240px] text-[28px] font-black text-light-50 md:w-[396px] md:text-center md:text-[44px] md:leading-[4rem] lg:w-[650px] lg:text-[64px]">
             Where would you like to go?
           </h1>
