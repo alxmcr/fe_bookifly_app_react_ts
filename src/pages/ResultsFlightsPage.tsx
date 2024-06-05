b7b268372cd0bdf3aee3af1b5cd22b5a8f0aa865
Alejandro Coca <alxmcr@outlook.com>
2024-06-04 20:18:32 -0400

    wip

--- src/pages/ResultsFlightsPage.tsx
+++ src/pages/ResultsFlightsPage.tsx
@@ -1,6 +1,18 @@
+import React from 'react';
 import { NavLink } from 'react-router-dom';
+import {
+  FlightCityFromSearchContext,
+  FlightCityToSearchContext,
+  FlightDepartureDateContext,
+  FlightPassengersContext,
+} from '../@providers/search/SearchFlightContext';
 
 export default function ResultsFlightsPage() {
+  const fromId = React.useContext(FlightCityFromSearchContext);
+  const toId = React.useContext(FlightCityToSearchContext);
+  const departureDate = React.useContext(FlightDepartureDateContext);
+  const passengers = React.useContext(FlightPassengersContext);
+
   return (
     <main>
       <div>
@@ -10,7 +22,19 @@ export default function ResultsFlightsPage() {
         <NavLink to="/flights">Flights page</NavLink>
         <NavLink to="/booking-confirmation">Booking confirmation page</NavLink>
       </div>
-      <h1>ResultsFlights Page</h1>
+      <h1>Flight Page</h1>
+      <pre>
+        {JSON.stringify(
+          {
+            fromId,
+            toId,
+            departureDate,
+            passengers,
+          },
+          null,
+          2,
+        )}
+      </pre>
     </main>
   );
 }
