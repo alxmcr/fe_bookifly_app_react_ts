a0593bcb6927b4049cd3bed5e6d30c6c065d5622
Alejandro Coca <alxmcr@outlook.com>
2024-06-05 19:31:07 -0400

    wip

--- src/services/local/flights/LocalFlightServiceImpl.ts
+++ src/services/local/flights/LocalFlightServiceImpl.ts
@@ -39,11 +39,11 @@ export class LocalFlightServiceImpl implements ILocalFlightService {
           );
 
           const filter = flights.filter((flight) => {
-            return (
-              flight.flight_from.cityId === fromId &&
-              flight.flight_to.cityId === toId &&
-              new Date(flight.date).getTime() === new Date(departDate).getTime()
-            );
+            const conditionByFrom = flight.flight_from.cityId === fromId;
+            const conditionByTo = flight.flight_to.cityId === toId;
+            const conditionByDate = new Date(flight.date).getTime() === new Date(departDate).getTime();
+
+            return conditionByFrom && conditionByTo && conditionByDate;
           });
 
           resolve(filter);
