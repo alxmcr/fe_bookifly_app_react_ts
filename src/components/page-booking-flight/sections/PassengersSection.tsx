1cb3b20259671bf37d88f00a3dcae8142da13fd3
Alejandro Coca <alxmcr@outlook.com>
2024-06-08 02:01:26 -0400

    wip

---
+++ src/components/page-booking-flight/sections/PassengersSection.tsx
@@ -0,0 +1,11 @@
+export default function PassengersSection() {
+  return (
+    <section className="w-full bg-[url('/assets/images/bg-main-bookifly.png')] bg-no-repeat py-10">
+      <div className="mx-auto h-screen lg:w-[1028px]">
+        <div className="flex h-screen flex-col gap-[50px] px-4 md:px-10">
+          <h2 className="font-nunito text-[22px] font-extrabold text-light-50">Passengers information</h2>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/BookingFlightPage.tsx
+++ src/pages/BookingFlightPage.tsx
@@ -1,9 +1,11 @@
 import MainContentBookingFlightPage from '../components/page-booking-flight/MainContentBookingFlightPage';
+import PassengersSection from '../components/page-booking-flight/sections/PassengersSection';
 
 export default function BookingFlightPage() {
   return (
     <div>
       <MainContentBookingFlightPage />
+      <PassengersSection />
     </div>
   );
 }
