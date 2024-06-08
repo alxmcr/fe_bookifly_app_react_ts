28bd63a289181fc5c6b7886cf562f1dd53e38453
Alejandro Coca <alxmcr@outlook.com>
2024-06-08 01:21:57 -0400

    wip

--- src/components/page-booking-flight/sections/HeroSectionBookingFlightPage.tsx
+++ src/components/page-booking-flight/sections/HeroSectionBookingFlightPage.tsx
@@ -11,29 +11,31 @@ export default function HeroSectionBookingFlightPage() {
   return (
     <section className="w-full bg-black py-[100px]">
       <div className="mx-auto h-[50px] lg:w-[1028px]">
-        <div className="flex flex-col gap-4 border border-riptide-200 p-4 md:w-[320px] md:px-10">
-          <div className="flex items-center justify-between gap-4">
-            <div className="flex items-center gap-2 text-riptide-200">
-              <Icon36x36Calendar />
-              <span className="font-nunito">08/06/2024</span>
-            </div>
-            <div className="flex items-center gap-2 text-riptide-200">
-              <Icon36x36AlarmOutlineRounded />
-              <span className="font-nunito">08/06/2024</span>
-            </div>
-          </div>
-          {fromId > 0 && toId > 0 ? (
+        <div className="flex items-center justify-center">
+          <div className="flex flex-col gap-4 border border-riptide-200 p-4 md:min-w-[320px] md:px-10">
             <div className="flex items-center justify-between gap-4">
-              <BoxCityHeader cityId={fromId} />
-              <div className="md:hidden lg:hidden">
-                <IllustrationRouteFillMobile />
+              <div className="flex items-center gap-2 text-riptide-200">
+                <Icon36x36Calendar />
+                <span className="font-nunito">08/06/2024</span>
               </div>
-              <div className="hidden md:flex">
-                <RouteIllustrationDesktop />
+              <div className="flex items-center gap-2 text-riptide-200">
+                <Icon36x36AlarmOutlineRounded />
+                <span className="font-nunito">08/06/2024</span>
               </div>
-              <BoxCityHeader cityId={toId} />
             </div>
-          ) : null}
+            {fromId > 0 && toId > 0 ? (
+              <div className="flex items-center justify-between gap-4">
+                <BoxCityHeader cityId={fromId} />
+                <div className="lg:hidden">
+                  <IllustrationRouteFillMobile />
+                </div>
+                <div className="hidden lg:flex">
+                  <RouteIllustrationDesktop />
+                </div>
+                <BoxCityHeader cityId={toId} />
+              </div>
+            ) : null}
+          </div>{' '}
         </div>
       </div>
     </section>
