70b990ae2334bddf198fb0dba4a981558ea3b84e
Alejandro Coca <alxmcr@outlook.com>
2024-06-09 13:41:28 -0400

    wip

---
+++ src/components/passengers/accordion-item-passengers/ItemAccordionPassenger.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  summary: string;
+  children: React.ReactNode;
+};
+
+export default function ItemAccordionPassenger({ summary = '', children }: Props) {
+  return (
+    <details>
+      <summary className="item-accordion-header list-none">{summary}</summary>
+      <div className="item-accordion-content">{children}</div>
+    </details>
+  );
+}
