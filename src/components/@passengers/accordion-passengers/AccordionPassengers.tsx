34720fbf00a7b08cb9ef2468a5011e19b14e5598
Alejandro Coca <alxmcr@outlook.com>
2024-06-09 14:39:24 -0400

    wip

--- src/components/@passengers/accordion-passengers/AccordionPassengers.tsx
+++ src/components/@passengers/accordion-passengers/AccordionPassengers.tsx
@@ -1,11 +1,28 @@
+import React from 'react';
+import { BookingContext, BookingDispatchContext } from '../../../@providers/booking/BookingContext';
+import { initialTemporalPassenger } from '../../../mocks/data/mock-provider-data';
+import { addPassengerAction } from '../../../store/booking/@actions/actions';
+import BoxFormPassenger from '../BoxFormPassenger';
 import ItemAccordionPassenger from '../accordion-item-passengers/ItemAccordionPassenger';
 import './AccordionPassengers.css';
 
 export default function AccordionPassengers() {
+  const booking = React.useContext(BookingContext);
+  const dispatchBooking = React.useContext(BookingDispatchContext);
+
+  React.useEffect(() => {
+    for (let index = 1; index <= booking.numberOfTickets; index++) {
+      addPassengerAction({ ...initialTemporalPassenger, pa_temporal_id: index }, dispatchBooking);
+    }
+  }, [booking.numberOfTickets, dispatchBooking]);
+
   return (
     <div className="accordion">
-      <ItemAccordionPassenger summary="Element #1">111111</ItemAccordionPassenger>
-      <ItemAccordionPassenger summary="Element #2">222222</ItemAccordionPassenger>
+      {booking.passengers.map((passenger, index) => (
+        <ItemAccordionPassenger key={passenger.pa_temporal_id} summary={`Passenger ${index + 1}`}>
+          <BoxFormPassenger />
+        </ItemAccordionPassenger>
+      ))}
     </div>
   );
 }
