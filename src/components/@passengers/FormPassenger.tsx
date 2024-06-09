b3cc28b88d2188c94b67f6c037cb6951121e33e9
Alejandro Coca <alxmcr@outlook.com>
2024-06-09 17:03:40 -0400

    wip

--- src/components/@passengers/FormPassenger.tsx
+++ src/components/@passengers/FormPassenger.tsx
@@ -1,24 +1,26 @@
 export default function FormPassenger() {
   return (
-    <form>
-      <label htmlFor="first-name">
+    <form className="flex flex-col gap-2">
+      <label htmlFor="first-name" className="flex flex-col gap-2">
         <span className="font-nunito text-light-50">First name:</span>
         <input
           type="text"
           name="first-name"
           id="first-name"
           placeholder="Enter your first name"
+          className="h-[40px] rounded-lg border border-riptide-200 bg-transparent px-[12px] py-[9px]"
           autoComplete="off"
           required
         />
       </label>
-      <label htmlFor="last-name">
+      <label htmlFor="last-name" className="flex flex-col gap-2">
         <span className="font-nunito text-light-50">Last name:</span>
         <input
           type="text"
           name="last-name"
           id="last-name"
           placeholder="Enter your last name"
+          className="h-[40px] rounded-lg border border-riptide-200 bg-transparent px-[12px] py-[9px]"
           autoComplete="off"
           required
         />
