021ebfda2ed9abd9eaf497d834c15aaeddd16535
Alejandro Coca <alxmcr@outlook.com>
2024-06-10 18:02:11 -0400

    wip

--- src/pages/ResultsFlightsPage.tsx
+++ src/pages/ResultsFlightsPage.tsx
@@ -1,4 +1,7 @@
 import BoxFlightsByAllConditions from '../components/@flights/BoxFlightsByAllConditions';
+import BoxFlightsByCityFrom from '../components/@flights/BoxFlightsByCityFrom';
+import BoxFlightsByCityTo from '../components/@flights/BoxFlightsByCityTo';
+import BoxFlightsByDepartureDate from '../components/@flights/BoxFlightsByDepartureDate';
 import MainContentResultsFlightsPage from '../components/page-results-flights/MainContentResultsFlightsPage';
 import FlightsSection from '../components/page-results-flights/sections/FlightsSection';
 
@@ -9,6 +12,15 @@ export default function ResultsFlightsPage() {
       <div className="h-screen w-full bg-[url('/assets/images/bg-main-bookifly.png')] bg-no-repeat">
         <FlightsSection title="Flighs">
           <BoxFlightsByAllConditions />
+        </FlightsSection>
+        <FlightsSection title="Flighs by city from">
+          <BoxFlightsByCityFrom />
+        </FlightsSection>
+        <FlightsSection title="Flighs by city to">
+          <BoxFlightsByCityTo />
+        </FlightsSection>
+        <FlightsSection title="Flighs by departure date">
+          <BoxFlightsByDepartureDate />
         </FlightsSection>
       </div>
     </div>
