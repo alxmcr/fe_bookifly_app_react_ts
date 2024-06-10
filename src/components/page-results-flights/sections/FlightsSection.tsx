01e0e39b6a84fa612872a69d0d43074f7b90fdfc
Alejandro Coca <alxmcr@outlook.com>
2024-06-10 17:58:29 -0400

    wip

--- src/components/page-results-flights/sections/FlightsSection.tsx
+++ src/components/page-results-flights/sections/FlightsSection.tsx
@@ -1,11 +1,17 @@
+import React from 'react';
 import BoxFlightsByAllConditions from '../../@flights/BoxFlightsByAllConditions';
 
-export default function FlightsSection() {
+type Props = {
+  title: string;
+  children: React.ReactNode;
+};
+
+export default function FlightsSection({ title = '' }: Props) {
   return (
     <section className="w-full bg-[url('/assets/images/bg-main-bookifly.png')] bg-no-repeat py-10">
       <div className="mx-auto h-screen lg:w-[1028px]">
         <div className="flex h-screen flex-col gap-[50px] px-4 md:px-10 lg:px-0">
-          <h2 className="font-nunito text-[22px] font-extrabold text-light-50">Flights</h2>
+          <h2 className="font-nunito text-[22px] font-extrabold text-light-50">{title}</h2>
           <BoxFlightsByAllConditions />
         </div>
       </div>
--- src/pages/ResultsFlightsPage.tsx
+++ src/pages/ResultsFlightsPage.tsx
@@ -1,3 +1,4 @@
+import BoxFlightsByAllConditions from '../components/@flights/BoxFlightsByAllConditions';
 import MainContentResultsFlightsPage from '../components/page-results-flights/MainContentResultsFlightsPage';
 import FlightsSection from '../components/page-results-flights/sections/FlightsSection';
 
@@ -6,7 +7,9 @@ export default function ResultsFlightsPage() {
     <div>
       <MainContentResultsFlightsPage />
       <div className="h-screen w-full bg-[url('/assets/images/bg-main-bookifly.png')] bg-no-repeat">
-        <FlightsSection />
+        <FlightsSection title="Flighs">
+          <BoxFlightsByAllConditions />
+        </FlightsSection>
       </div>
     </div>
   );
