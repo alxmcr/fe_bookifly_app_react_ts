9117e09249cec1bb5899591bf81a91d21b87edfa
Alejandro Coca <alxmcr@outlook.com>
2024-06-10 18:27:05 -0400

    wip

--- src/components/page-home/sections/HeroSectionHomePage.tsx
+++ src/components/page-home/sections/HeroSectionHomePage.tsx
@@ -4,7 +4,7 @@ export default function HeroSectionHomePage() {
   return (
     <section className="w-full bg-[url('/assets/images/bg-main-bookifly.png')] bg-no-repeat py-10">
       <div className="mx-auto h-screen lg:w-[1028px]">
-        <div className="flex h-screen flex-col justify-center gap-[50px] px-4 md:items-center md:px-10">
+        <div className="flex h-screen flex-col justify-center gap-[50px] px-4 md:items-center md:px-10 lg:px-0">
           <h1 className="w-[240px] text-[28px] font-black text-light-50 md:w-[396px] md:text-center md:text-[44px] md:leading-[4rem] lg:w-[650px] lg:text-[64px]">
             Where would you like to go?
           </h1>
