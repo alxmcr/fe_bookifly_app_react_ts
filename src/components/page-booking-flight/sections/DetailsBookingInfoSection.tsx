2710b67138ecdb991030e4b11cce4a0f00441840
Alejandro Coca <alxmcr@outlook.com>
2024-06-10 23:58:27 -0400

    wip

--- src/components/page-booking-flight/sections/DetailsBookingInfoSection.tsx
+++ src/components/page-booking-flight/sections/DetailsBookingInfoSection.tsx
@@ -28,9 +28,12 @@ export default function DetailsBookingInfoSection() {
   return (
     <section className="min-h-screen w-full bg-[url('/assets/images/bg-main-bookifly.png')] bg-cover bg-no-repeat py-10">
       <div className="mx-auto lg:w-[1028px]">
-        <div className="flex flex-col gap-4 md:grid md:grid-cols-2">
-          <BoxBookingPassengers />
-          <BoxBookingCheckoutInfo />
+        <div className="flex flex-col gap-4">
+          <h2 className="text-[26px] font-bold text-riptide-200">Details booking</h2>
+          <div className="flex flex-col gap-4 md:grid md:grid-cols-2">
+            <BoxBookingPassengers />
+            <BoxBookingCheckoutInfo />
+          </div>
         </div>
       </div>
     </section>
