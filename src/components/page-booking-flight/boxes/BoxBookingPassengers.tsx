8c3d49326a067c30fb73ec8052d8ca5ef26b37fd
Alejandro Coca <alxmcr@outlook.com>
2024-06-10 20:02:04 -0400

    wip

--- src/components/page-booking-flight/boxes/BoxBookingPassengers.tsx
+++ src/components/page-booking-flight/boxes/BoxBookingPassengers.tsx
@@ -6,7 +6,7 @@ export default function BoxBookingPassengers() {
   const booking = React.useContext(BookingContext);
 
   return (
-    <div className="flex flex-col gap-[50px] rounded-lg bg-black px-4 py-2 md:px-10 lg:px-0">
+    <div className="flex flex-col gap-[50px] rounded-2xl bg-black px-4 py-7 md:px-10">
       <h2 className="font-nunito text-[22px] font-extrabold text-riptide-200">
         Passengers information ({booking.numberOfTickets})
       </h2>
