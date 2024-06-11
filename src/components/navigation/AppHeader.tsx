dd3b1f592a7f0919488c9dd54f427062ddac41c8
Alejandro Coca <alxmcr@outlook.com>
2024-06-10 22:55:15 -0400

    wip

--- src/components/navigation/AppHeader.tsx
+++ src/components/navigation/AppHeader.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import Logo56x56Bookifly from '../@logos/56x56/Logo56x56Bookifly';
 
 export default function AppHeader() {
@@ -5,8 +6,10 @@ export default function AppHeader() {
     <header className="absolute flex w-full justify-center py-4">
       <div className="mx-auto flex w-full items-center justify-between px-4 md:px-10 lg:w-[1028px] lg:px-0">
         <div className="flex items-center gap-4">
-          <Logo56x56Bookifly />
-          <h2 className="font-inter text-[20px] font-bold text-light-50">Bookifly</h2>
+          <NavLink to="/" className="flex items-center gap-4">
+            <Logo56x56Bookifly />
+            <h2 className="font-inter text-[20px] font-bold text-light-50">Bookifly</h2>
+          </NavLink>
         </div>
       </div>
     </header>
