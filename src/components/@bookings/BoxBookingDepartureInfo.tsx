3a992259d3eb352f6566c2b00188818fc3bffe25
Alejandro Coca <alxmcr@outlook.com>
2024-06-11 19:38:48 -0400

    wip

--- src/components/@bookings/BoxBookingDepartureInfo.tsx
+++ src/components/@bookings/BoxBookingDepartureInfo.tsx
@@ -1,37 +1,22 @@
-import { LoadingStates } from '../../@types/service/enumsService';
-import { useLocalFlight } from '../../hooks/flights/local/useFlight';
+import { LocalFlightData } from '../../@types/service/serviceTypes';
 import Icon36x36AlarmOutlineRounded from '../@icons/36x36/Icon36x36AlarmOutlineRounded';
 import Icon36x36Calendar from '../@icons/36x36/Icon36x36Calendar';
 
 type Props = {
-  flightId: string;
+  flight: LocalFlightData;
 };
 
-export default function BoxBookingDepartureInfo({ flightId }: Props) {
-  const { flight, errorFlight, statusFlight } = useLocalFlight(flightId);
-
-  if (LoadingStates.PENDING === statusFlight) {
-    return <p>Loading Flight...</p>;
-  }
-
-  if (LoadingStates.ERROR === statusFlight && errorFlight) {
-    return <p>{errorFlight.message}</p>;
-  }
-
-  if (LoadingStates.SUCCESS === statusFlight && flight) {
-    return (
-      <>
-        <div className="flex items-center gap-2 text-riptide-200">
-          <Icon36x36Calendar />
-          <span className="font-nunito">{flight.departureDate}</span>
-        </div>
-        <div className="flex items-center gap-2 text-riptide-200">
-          <Icon36x36AlarmOutlineRounded />
-          <span className="font-nunito">{flight.departureTime}</span>
-        </div>
-      </>
-    );
-  }
-
-  return null;
+export default function BoxBookingDepartureInfo({ flight }: Props) {
+  return (
+    <>
+      <div className="flex items-center gap-2 text-riptide-200">
+        <Icon36x36Calendar />
+        <span className="font-nunito">{flight.departureDate}</span>
+      </div>
+      <div className="flex items-center gap-2 text-riptide-200">
+        <Icon36x36AlarmOutlineRounded />
+        <span className="font-nunito">{flight.departureTime}</span>
+      </div>
+    </>
+  );
 }
--- src/components/@flight-route/BoxFlightRoute.tsx
+++ src/components/@flight-route/BoxFlightRoute.tsx
@@ -1,38 +1,23 @@
-import { LoadingStates } from '../../@types/service/enumsService';
-import { useLocalFlight } from '../../hooks/flights/local/useFlight';
+import { LocalFlightData } from '../../@types/service/serviceTypes';
 import BoxCityHeader from '../@cities/BoxCityHeader';
 import IllustrationRouteFillDesktop from '../@illustrations/IllustrationRouteFillDesktop';
 import IllustrationRouteFillMobile from '../@illustrations/IllustrationRouteFillMobile';
 
 type Props = {
-  flightId: string;
+  flight: LocalFlightData;
 };
 
-export default function BoxFlightRoute({ flightId = '' }: Props) {
-  const { flight, errorFlight, statusFlight } = useLocalFlight(flightId);
-
-  if (LoadingStates.PENDING === statusFlight) {
-    return <p>Loading Flight...</p>;
-  }
-
-  if (LoadingStates.ERROR === statusFlight && errorFlight) {
-    return <p>{errorFlight.message}</p>;
-  }
-
-  if (LoadingStates.SUCCESS === statusFlight && flight) {
-    return (
-      <div className="flex items-center justify-between gap-4">
-        <BoxCityHeader cityId={flight.city_departure_from.cityId} />
-        <div className="lg:hidden">
-          <IllustrationRouteFillMobile />
-        </div>
-        <div className="hidden lg:flex">
-          <IllustrationRouteFillDesktop />
-        </div>
-        <BoxCityHeader cityId={flight.city_destination_to.cityId} />
+export default function BoxFlightRoute({ flight }: Props) {
+  return (
+    <div className="flex items-center justify-between gap-4">
+      <BoxCityHeader cityId={flight.city_departure_from.cityId} />
+      <div className="lg:hidden">
+        <IllustrationRouteFillMobile />
       </div>
-    );
-  }
-
-  return null;
+      <div className="hidden lg:flex">
+        <IllustrationRouteFillDesktop />
+      </div>
+      <BoxCityHeader cityId={flight.city_destination_to.cityId} />
+    </div>
+  );
 }
